9ecf56edd834902f6665a1539501d755ba1f1e2f
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2023-04-05 02:40:41 +0200

    Adds a bat file for converting aud files to mp3

---
+++ Soundtrack-info/aud.to.mp3.bat
@@ -0,0 +1,22 @@
+:: Proof of concept with USRDIR/PocketDJ/antiflag.aud
+
+echo "Moves the file we want to convert to the working directory"
+MOVE antiflag.aud tools.required/
+
+echo "Makes sure the aud file is an .at3/aa3 valid and changes it to .aa3"
+cd tools.required/
+quickbms.exe converter.bms antiflag.aud
+
+:: echo "File.aud to file.aa3"
+:: at3tool.exe
+
+ffmpeg -i antiflag.aa3 antiflag.mp3
+echo "Conversion completed!"
+
+echo "Clean temp files, erasing .aa3 file"
+DEL antiflag.aa3
+
+echo "Returns source and converted file to main directory:"
+MOVE antiflag.aud ../
+MOVE antiflag.mp3 ../
+pause
