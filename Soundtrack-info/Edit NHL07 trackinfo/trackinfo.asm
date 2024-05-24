8b62a2f275f3dc93414dd64617a274f45975f063
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2024-05-25 00:51:36 +0200

    Now there're 2 songs in the asm

--- Soundtrack-info/Edit NHL07 trackinfo/trackinfo.asm
+++ Soundtrack-info/Edit NHL07 trackinfo/trackinfo.asm
@@ -29,19 +29,29 @@
     song_002:
     .string "GoodNightNurse"	; artist name
 	.byte 0
+	.string "Goodnight.Nurse" 	; artist name
+	.byte 0
+    .string "My.Only"	; song name	
+	.byte 0
+    .string "Always.And.Never"	; album's name 
+	.byte 0
+
+	.halfword 0
+	
+
 
 
 ; test to add more songs Currently not working
-.org 0x092D
-    song_00X:
-    .string "BloodPit"	; audio's file name for the track
-	.byte 0
-    .string "Bloodpit" 	; artist name
-	.byte 0
-    .string "Platitude"	; song name	
-	.byte 0
-    .string "Mental.Circus"	; 
-	.byte 0
-	.halfword 0
+;.org 0x092D
+;    song_00X:
+;    .string "BloodPia"	; audio's file name for the track
+;	.byte 0
+;    .string "Bloodpit" 	; artist name
+;	.byte 0
+;    .string "Platitude"	; song name	
+;	.byte 0
+;    .string "Mental.Circus"	; 
+;	.byte 0
+;	.halfword 0
 	
 .close
