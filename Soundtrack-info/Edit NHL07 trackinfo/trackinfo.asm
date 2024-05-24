a2380575b10e88f18bc330074d2555557c4ca0d5
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2024-05-25 01:06:40 +0200

    add 5th song

--- Soundtrack-info/Edit NHL07 trackinfo/trackinfo.asm
+++ Soundtrack-info/Edit NHL07 trackinfo/trackinfo.asm
@@ -60,6 +60,18 @@
     .string "Hello.Master"	; album's name 
 	.byte 0
 
+	.halfword 0
+	
+    song_005:
+    .string "Mobile"	; audio's file name for the track
+	.byte 0
+	.string "Mobile" 	; artist name
+	.byte 0
+    .string "Montreal.Calling"	; song name	
+	.byte 0
+    .string "Tomorrow.Starts.Today"	; album's name 
+	.byte 0
+
 ; test to add more songs Currently not working
 ;.org 0x092D
 ;    song_00X:
