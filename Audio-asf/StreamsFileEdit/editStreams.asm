7b2accbf7b7912f9c374635634204d0ca9b9f25a
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2024-05-28 17:42:29 +0200

    you can change any audio now

--- Audio-asf/StreamsFileEdit/editStreams.asm
+++ Audio-asf/StreamsFileEdit/editStreams.asm
@@ -1,4 +1,20 @@
 ; Author: Bunkai
+; following code uses data from vanilla, if you updated any audio from this file before
+; you need to update that info in this asm file too.
+
+; example to (re)instert the audiofile "AllStarIntroNHL07_1.asf"
+; is recommended to use files that are shorter or equal size, but you can insert bigger files
+
+fileOffset equ 0x09910EC0	; in little endian is (0xC00E9109)
+
+.psp
+.open "streams.viv" , 0x0
+.org fileOffset
+.import "AllStarIntroNHL07_1.asf"
+.close
+
+; After inserting the audio file, it's turn to edit the offsets if they have changed
+; if they were shorter or equal you can leave them as they were.
 
 .psp
 .open "streams.viv" , 0x0
@@ -13,18 +29,14 @@
 
 ; audio files, with 0x0000 (.halfword 0) as delimiter after each section
 
-.word 0xC00E9109	; asf audio file offset, in little endian (0x 09 91 0E C0)
-.word 0x142C1900	; asf audio file size, in little endian (0x 00 19 2C 14)
+.word 0xC00E9109	; asf audio file offset, in little endian, (0x 09 91 0E C0)
+.word 0x142C1900	; asf audio file size, in little endian, (0x 00 19 2C 14)
 .ASCIIZ "AllStarIntroNHL07_1.asf" ; asf file name + delimiter (0x0)
 
-.word 0x00311900	; asf audio file offset, in little endian (0x 00 19 31 00)
-.word 0x142C1900	; asf audio file size, in little endian (0x 00 19 2C 14)
+.word 0x00311900	; asf audio file offset, in little endian, (0x 00 19 31 00)
+.word 0x142C1900	; asf audio file size, in little endian, (0x 00 19 2C 14)
 .ASCIIZ "AllStarIntroNHL07_2.asf" ; asf file name + delimiter (0x0)
 
 ; same format for all the remaining files
-
-; if you want to reinsert an asf file in a section:
-; remember to change the offsets for the files that come after that one,
-; or make sure your inserted file is shorter or equal size than the original
 
 .close
