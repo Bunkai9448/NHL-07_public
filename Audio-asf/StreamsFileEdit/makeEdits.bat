3147b25de7d71326c51a41ddbc1e6dc30b7efca6
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2024-05-28 18:22:30 +0200

    automate the wav to asf conversion too

--- Audio-asf/StreamsFileEdit/makeEdits.bat
+++ Audio-asf/StreamsFileEdit/makeEdits.bat
@@ -1,6 +1,12 @@
+:: Convert the WAV file to ASF
+EA_sx2 -pspstream -eaxa_blk -rs44100 source.wav -=output.asf
+
+:: Make a back up copy from streams.viv, in case something goes wrong
 IF NOT EXIST "streams.viv_BackUp" (
 	copy streams.viv streams.viv_BackUp
 )
+
+:: Insert the new audio and fix the offsets if needed
 armips editStreams.asm
 
 PAUSE
