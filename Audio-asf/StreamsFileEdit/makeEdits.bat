804030f49b8c0ac1d2d32043c2d0d24a21ec6f38
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2024-05-30 15:42:22 +0200

    pause to obtain your asf data

--- Audio-asf/StreamsFileEdit/makeEdits.bat
+++ Audio-asf/StreamsFileEdit/makeEdits.bat
@@ -1,6 +1,8 @@
 :: Convert the WAV file to ASF
 EA_sx2 -pspstream -eaxa_blk -rs44100 source.wav -=output.asf
 
+PAUSE
+
 :: Make a back up copy from streams.viv, in case something goes wrong
 IF NOT EXIST "streams.viv_BackUp" (
 	copy streams.viv streams.viv_BackUp
