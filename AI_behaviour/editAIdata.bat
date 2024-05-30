d8375929e2c32cf09bc59ed9e9b204df8f4eb1fe
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2024-05-30 15:46:45 +0200

    Delete AI_behaviour/editAIdata.bat

--- AI_behaviour/editAIdata.bat
+++
@@ -1,9 +0,0 @@
-:: Make a back up copy from streams.viv, in case something goes wrong
-IF NOT EXIST "Difficlt.ubc_BackUp" (
-	copy Difficlt.ubc Difficlt.ubc_BackUp
-)
-
-:: Insert the new audio and fix the offsets if needed
-armips Difficult.asm
-
-PAUSE
