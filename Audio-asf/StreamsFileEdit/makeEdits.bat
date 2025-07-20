dfa85cfdd600174a57e6017c3ec17616ae8cd5c1
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2025-07-20 19:59:45 +0200

    use source file, not a modified one

--- Audio-asf/StreamsFileEdit/makeEdits.bat
+++ Audio-asf/StreamsFileEdit/makeEdits.bat
@@ -6,6 +6,8 @@ PAUSE
 :: Make a back up copy from streams.viv, in case something goes wrong
 IF NOT EXIST "streams.viv_BackUp" (
 	copy streams.viv streams.viv_BackUp
+) ELSE ( :: If there was a backup it means we have to use that instead
+    copy streams.viv_BackUp streams.viv
 )
 
 :: Insert the new audio and fix the offsets if needed
